00dcccb853091cd5ef72f04731d22d972b753ee0
FaroukAhmad <farouqgt@gmail.com>
2022-09-12 16:32:36 -0700

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,7 +18,8 @@ int main(void)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			if ((i < j) & (j <= '9'))														{
+			if ((i < j) & (j <= '9'))
+			{
 				putchar(i);
 				putchar(j);
 				if ((j < '9') | (i < '8'))
