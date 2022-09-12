da9dacc077edf1e70e89211097856afba0fbcea1
FaroukAhmad <farouqgt@gmail.com>
2022-09-11 22:10:39 -0700

    my commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * return: Always 0 (succes)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch != 'e' && ch != 'q')
+		putchar(ch);
+	}
+		putchar('\n');
+	return(0);
+}
