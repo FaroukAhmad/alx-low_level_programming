779f011d1b1008e6ed247ae423a707dbb89afa5e
FaroukAhmad <farouqgt@gmail.com>
2022-09-12 15:34:33 -0700

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 /**
  * main - entry point
  *
@@ -6,6 +6,7 @@
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1,
+						stderr);
 	return (1);
 }
