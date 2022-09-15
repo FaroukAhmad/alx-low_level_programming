f9cae336d547c01feed5c765878dfdf7aa42e69e
FaroukAhmad <farouqgt@gmail.com>
2022-09-15 15:26:40 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - function that multifly two given integers
+ * @a: one of the integers
+ * @b: one of the  integers
+ * return: 0 success
+*/
+int mul(int a, int b)
+{
+	int result;
+	result = (a * b);
+	return (result);
+}
