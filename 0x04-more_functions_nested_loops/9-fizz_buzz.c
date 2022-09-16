7bb4e253304d10fb158a515a326d75052b3ebbb3
FaroukAhmad <farouqgt@gmail.com>
2022-09-15 22:45:53 -0700

    my commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,19 +6,19 @@
 */
 int main(void)
 {
-int i =  1;
+	int i =  1;
 
-for (; i < 100; i++)
-{
-if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz");
-else if (i % 3 == 0)
-printf("Fizz");
-else if (1 % 5 == 0)
-printf("Buzz");
-else
-printf("%d", i);
-}
-printf("Buzz\n");
-return (0);
+	for (; i < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (1 % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+	}
+	printf("Buzz\n");
+	return (0);
 }
