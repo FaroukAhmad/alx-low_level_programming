9939a8276f7d3f6c93e7b3b092a4d33ab25ae72c
FaroukAhmad <farouqgt@gmail.com>
2022-09-15 20:44:42 -0700

    my commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 	int i;
 	int n;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (n = '0'; n <= '14'; n++)
+		for (n = 0; n <= 14; n++)
 		{
 			if (n > 9)
 			_putchar((n / 10) + '0');
