a0b6397550b784921caf679564df0ab00a0d9edd
FaroukAhmad <farouqgt@gmail.com>
2022-09-19 14:29:56 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - writes half of a charcter string
+ * @str: input string to print
+ * Description: print the half charcter of string
+ * Return: 0 success
+*/
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+		j = i / 2;
+		if (i % 2 == 0)
+		{
+			j++;
+		}
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/EWADME.md
@@ -0,0 +1,1 @@
+Write a function that prints half of a string, followed by a new line.
