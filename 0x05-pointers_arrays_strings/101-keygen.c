1c1dc2e3b46a0b1f478d41ef1fa54ee1c3b73d61
FaroukAhmad <farouqgt@gmail.com>
2022-09-19 16:24:16 -0700

    my key_gen commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - program that generates random valid
  * passwords for the program 101-crackme
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	int pass[100];
 	int i, sum, n;
 
-	sum = 0;	
-
+	sum = 0;
 	srand(time(NULL));
-
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
@@ -30,6 +27,5 @@ int main(void)
 			break;
 		}
 	}
-
 	return (0);
 }
