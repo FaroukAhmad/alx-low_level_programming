8d539d4efec9ef0bed9245c851138fec2ecdd0cc
Umar Farouk <78317501+FaroukAhmad@users.noreply.github.com>
2022-10-15 12:00:02 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,59 +1,48 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "variadic_functions.h"
 
-void i_print(va_list i)
-{
-	printf("%d", va_arg(i, int);
-}
-void c_print(va_list i)
-{
-	printf("%c", va_arg(i, char);
-}
-void f_print(va_list i)
-{
-	printf("%f", va_arg(i, float);
-}
-void s_print(va_list i)
-{
-	if (va_arg(i, char *) == NULL)
-		printf("(nil");
-	else
-		printf("%s", va_arg(i, char *);
-}
-typedef struct print
-{
-	char var;
-	void (*func)(va_list);
-}print_t;
-void print_one(char a, va_list ap)
-{
-	int i;
-	print_t list[] = {
-		{'c', i_print},
-		{'i', c_print},
-		{'f', f_print},
-		{'s', s_print},
-		{NULL, NULL},
-	};
-	i = 0;
-	while (i < 4)
-	{
-		if (a = list.var[i])
-			list.func[i](ap);
-		i++;
-	}
-}
 /**
- * print_all - print any type
- * @format - type of variable to print
- * Return: void
- **/
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
 void print_all(const char * const format, ...)
 {
-	int i;
+	int i = 0;
+	char *str, *sep = "";
 
-	i = 0;
-	while (i < for
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+
+	printf("\n");
+	va_end(list);
 }
